754ceefea2631199b267af95c08f1815409a2b40
Joy Serquiña <stepjoyous@gmail.com>
2023-02-15 16:25:47 -0800

    Removed generic data and passed customized props into Products and implemented props in Product component.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,17 @@
 import './Product.css'
 
-function Product() {
+function Product({ title, price, rating, imageUrl }) {
   return (
     <div className="product">
       <div className="product__details">
-        <p className="product__title">Product Title</p>
-        <p className="product__price">$30.00</p>
-        <div className="product__rating">⭐️ ⭐️ ⭐️ ⭐️</div>
+        <p className="product__title">{ title }</p>
+        <p className="product__price">${ price }</p>
+        <div className="product__rating">{
+          Array(rating).fill().map((_, i) => (
+            <p>⭐️</p>
+        ))}</div>
       </div>
-      <img src="https://loremflickr.com/320/240/dog" alt="dog" className="product__image" />
+      <img src={ imageUrl } alt={ title } className="product__image" />
       <button>Add to Basket</button>
     </div>
   )
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,16 +9,41 @@ function Home() {
       <div className="home__container">
         <img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="E-Commerce Hero Img" className="home__image" />
         <div className="home__row">
-          <Product/>
-          <Product/>
+          <Product
+            title="Ramsay in 10 Hardcover – November 2, 2021
+            by Gordon Ramsay  (Author)"
+            price={26.60}
+            rating={5}
+            imageUrl="https://m.media-amazon.com/images/P/1538707810.01._SCLZZZZZZZ_SX500_.jpg"/>
+          <Product
+            title="FLYBIRD Adjustable Bench, Utility Weight Bench for Full Body Workout- Multi-Purpose Foldable Incline/Decline Benchs, Regular/Roman Chair"
+            price={145.99}
+            rating={5}
+            imageUrl="https://m.media-amazon.com/images/I/61LCzHjGfRL._AC_SL1500_.jpg"/>
         </div>
         <div className="home__row">
-          <Product/>
-          <Product/>
-          <Product/>
+          <Product
+            title="HOOMEET 24 Pack Silicone Cupcake Liners, Non-Stick Muffin Liners, Silicone Muffin Cups, Silicone Baking Cups, Reusable cupcake liners for baking, Red Blue Green Yellow Pink Orange Color"
+            price={9.99}
+            rating={5}
+            imageUrl="https://m.media-amazon.com/images/I/71hV3keRRrL._AC_SL1500_.jpg"/>
+          <Product
+            title="Om Mushroom Superfood Master Blend Mushroom Powder Supplement, 3.17 Ounce, 34 Servings, 10 Mushroom Complex, Lions Mane, Chaga, Cordyceps, Reishi Extract Adaptogens for Vibrant Health, Immune Support"
+            price={16.99}
+            rating={3}
+            imageUrl="https://m.media-amazon.com/images/I/81Zm6JUAneL._AC_SL1500_.jpg"/>
+          <Product
+            title="Surge Protector Power Strip - 8 Widely Outlets with 4 USB Charging Ports, 3 Side Outlet Extender with 5Ft Braided Extension Cord, Flat Plug, Wall Mount, Desk USB Charging Station for Home Office ETL"
+            price={17.99}
+            rating={5}
+            imageUrl="https://m.media-amazon.com/images/I/61ZbXVoHlDL._AC_SL1500_.jpg"/>
         </div>
         <div className="home__row">
-          <Product/>
+          <Product
+            title={`Amazon Fire TV 43" 4-Series 4K UHD smart TV, stream live TV without cable`}
+            price={269.99}
+            rating={4}
+            imageUrl="https://m.media-amazon.com/images/I/51EOfJxElNL._AC_.jpg"/>
         </div>
       
       </div>
