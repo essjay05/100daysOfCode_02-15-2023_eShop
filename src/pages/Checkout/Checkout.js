faff1c2d3125827f1707e60afe8ae53595a01cb1
Joy Serquiña <stepjoyous@gmail.com>
2023-02-16 21:30:47 -0800

    Added button to subtotal and updated Subtotal and checkoutProduct.css files.

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -5,20 +5,18 @@ import Subtotal from './Subtotal'
 function Checkout() {
   return (
     <main className='checkout'>
-      <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="Amazon ad" className="checkout__ad" />
-      <section className='checkout__container'>
-        <div className="checkout__left">
-          <div>
-            <h2 className="checkout__title">
-              Your Shopping Basket
-            </h2>
-            <CheckoutProduct/>
-          </div>
+      <div className="checkout__left">
+        <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="Amazon ad" className="checkout__ad" />
+        <div>
+          <h2 className="checkout__title">
+            Your Shopping Basket
+          </h2>
+          <CheckoutProduct/>
         </div>
-        <div className="checkout__right">
-          <Subtotal />
-        </div>
-      </section>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
     </main>
   )
 }
--- src/pages/Checkout/Subtotal.js
+++ src/pages/Checkout/Subtotal.js
@@ -11,6 +11,7 @@ function Subtotal() {
       <small className='subtotal__gift'>
         <input type='checkbox' /> This order contains a gift.
       </small>
+      <button>Proceed to Checkout</button>
     </div>
   )
 }
